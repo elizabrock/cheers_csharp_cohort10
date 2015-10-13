0c17afc0120896a61ef44bd0d719649ada3920a5
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2015-10-13 13:44:37 -0600

    In-class work on the cheers exercise

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello there! What's your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Hi, " + name);
+            foreach(char letter in name)
+            {
+                System.Console.WriteLine("Give me " + letter);
+            }
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+            //Get the user's name and print it.
+            //Cheer the user on.
+            //For example, if name is "Bob", output should be:
+
+            //Give me a..b
+            //Give me an..o
+            //Give me a..b
+            //BOB is.. GRAND!
+        }
+    }
+}
