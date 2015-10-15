2ade8e87f41f53c4e1dec73a48a08ac3043e4ee3
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2015-10-15 10:15:29 -0600

    Cheers Part 1 in-class solution

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -13,19 +13,29 @@ namespace Cheers
             System.Console.WriteLine("Hello there! What's your name?");
             string name = System.Console.ReadLine();
             System.Console.WriteLine("Hi, " + name);
-            foreach(char letter in name)
+            foreach (char letter in name.ToLower())
             {
-                System.Console.WriteLine("Give me " + letter);
+                if (Char.IsLetter(letter))
+                {
+                    string mnemonic = "halfnorsemix";
+                    string article = "a";
+                    if (mnemonic.Contains(letter))
+                    {
+                        article = "an";
+                    }
+                    System.Console.WriteLine("Give me " + article + "..." + letter);
+                }
             }
+            System.Console.WriteLine(name.ToUpper() + " is.. GRAND!");
             System.Console.WriteLine("Press any key to exit");
             System.Console.ReadKey();
             //Get the user's name and print it.
             //Cheer the user on.
             //For example, if name is "Bob", output should be:
 
-            //Give me a..b
-            //Give me an..o
-            //Give me a..b
+            //Give me a...  b
+            //Give me an... o
+            //Give me a...  b
             //BOB is.. GRAND!
         }
     }
